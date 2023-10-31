90a039a4354cd36b42846b5fe4434e976cc5c1a1
LudaTverday <Ludachka22@gmail.com>
2023-10-31 10:02:50 +0200

    solution for BeforeEach annotation

--- src/main/java/telran/test/TestRunner.java
+++ src/main/java/telran/test/TestRunner.java
@@ -6,11 +6,9 @@ import java.util.*;
 
 import telran.test.annotation.*;
 
-
 public class TestRunner implements Runnable {
 	private Object testObj;
-	private List<Method> testMethods = new ArrayList<>();
-	private List<Method> beforeEachMethods = new ArrayList<>();
+	
 
 	public TestRunner(Object testObj) {
 		this.testObj = testObj;
@@ -20,49 +18,37 @@ public class TestRunner implements Runnable {
 	public void run() {
 		Class<?> clazz = testObj.getClass();
 		Method[] methods = clazz.getDeclaredMethods();
-		setMethodsByAnnotation(methods);
-		launchAllMethods();
+		Method[] beforeEachMethods = getBaforeEachMethods(methods);
+		runTestMethods(methods, beforeEachMethods);
 	}
-	
-	private void setMethodsByAnnotation(Method[] methods) {
+
+	private void runTestMethods(Method[] methods, Method[] beforeEachMethods) {
 		for (Method m : methods) {
-			if (m.isAnnotationPresent(Test.class)) {				
-					testMethods.add(m);
-			}
-			if(m.isAnnotationPresent(BeforeEach.class)) {
-				beforeEachMethods.add(m);
-			}
-		}
-	}
-
-	private void launchAllMethods() {
-		if(!testMethods.isEmpty()) {
-			for(Method m: testMethods) {
-			if(beforeEachMethods.size() != 0) {
-				lauchMethodsByAnnotation(beforeEachMethods);
-			}
-			try {
+			if (m.isAnnotationPresent(Test.class)) {
 				m.setAccessible(true);
-				m.invoke(testObj);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				System.out.println("Error: " + e.getMessage());
+				runMethods(beforeEachMethods);
+				try {
+					m.invoke(testObj);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
 			}
 		}
-		}		
 	}
 
-
-	private void lauchMethodsByAnnotation(List<Method> methods) {
-		for(Method method: methods) {
+	private void runMethods(Method[] methods) {
+		for (Method method : methods) {
 			try {
 				method.setAccessible(true);
 				method.invoke(testObj);
 			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				System.out.println("Error: " + e.getMessage());
+				throw new RuntimeException(e);
 			}
 		}
 	}
 
+	private Method[] getBaforeEachMethods(Method[] methods) {
 
-	
+		return Arrays.stream(methods).filter(m -> m.isAnnotationPresent(BeforeEach.class)).toArray(Method[]::new);
+	}
 }
